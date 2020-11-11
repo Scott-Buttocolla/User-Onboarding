02793b29587f241fe243453b6e093d784eabd6e5
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-11-11 12:32:48 -0800

    had to reforked, cloned lambdas file and not my own, Completed MVP

---
+++ user-onboarding/src/components/formSchema.js
@@ -0,0 +1,29 @@
+// adding yup and forming the structure for the schema
+// calling each field
+
+import * as yup from 'yup';
+
+export default yup.object().shape({
+    name: yup
+        .string()
+        .required('Name is Required')
+        .min(5, 'name must be longer then 5 characters'),
+    email: yup
+        .string()
+        .email('Must be a valid email address')
+        .required('Must include email address'),
+    password: yup
+        .string()
+        .min(7, 'Password must be at least 7 characters long')
+        .required('Password is Required'),
+    position: yup
+        .string()
+        .oneOf(['Team Leader', 'Frontend Engineer', 'Backend Engineer', 'UI/UX Designer'], 'You must select a position')
+        .required('You must select a position'),
+    title: yup
+        .string()
+        .required("Must select one"),
+    termsOfService: yup
+        .string()
+        .required("Must check")
+})
---
+++ user-onboarding/src/components/users.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Users(props) {
+    const { users } = props;
+    return users.map(user => {
+        return (
+            <>
+            <h2>{ user.title} {user.name}</h2>
+            <p>Email: {user.email}</p>
+            <p>Postion: {user.position}</p>
+            <p>Terms of Service: {user.termsOfService}</p>
+            </>
+        )
+    })
+}
